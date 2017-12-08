ce59202e34236f86d411e2e550e5541a839b641d
BobDorland <33722105+BobDorland@users.noreply.github.com>
2017-12-08 12:36:28 +0100

    Add files via upload

---
+++ HabitServer.js
@@ -0,0 +1,88 @@
+import { setInterval } from "timers";
+
+var http = require("http");                                     // retrieves HTTP package
+var server;   
+var app;
+
+server = http.createServer(function(req, res) {                  // creates a web server 
+
+    res.writeHead(200, {"Content-Type" : 
+                                          "text/plain"});        // creates a HTTP response & sends it 
+            res.end("Processed.");                               
+
+    console.log("HTTP response was sent.");
+                                                                
+});
+
+var port = 2345;                                                 // accepts ports between 1024 and 49151
+server.listen(port);                                             // creates a server that listens on the given port
+console.log("Server is listening on port" + port);
+
+
+var habits = [];                                                 // in memory list of habits 
+
+function addHabit(habit) {                                       // add a habit to the memory of the server
+
+    habits.push(habit);
+
+}
+
+function removeHabit(habit) {                                    // remove a habit from the memory of the server
+    
+            var index = habits.indexOf(habit);
+    
+            if(index >= 0) {
+    
+                habits.splice(index, 1);
+    
+            }
+    
+}
+
+function updateHabitName(habit, newName) {                      // updates the name of a habit
+
+    habit.setName(newName);
+
+}
+
+function updateHabitDays(habit, newDays) {                      // updates the days a habit is planned
+
+    habit.setDays(newDays);
+
+}
+
+app.get("/habits", function(reg, req) {                          // sends JSON to the client
+
+    res.json(habits);
+
+});   
+
+setInterval(function() {                                        // retrieves a list of habits from the server every 2 seconds (addHabit)
+
+    console.log("Fetching the habit list from the server.");
+    $.getJSON(habits, addHabit());
+
+}, 2000);
+
+setInterval(function() {                                        // retrieves a list of habits from the server every 2 seconds (removeHabit)
+    
+    console.log("Fetching the habit list from the server.");
+    $.getJSON(habits, removeHabit());
+    
+}, 2000);
+
+setInterval(function() {                                        // retrieves a list of habits from the server every 2 seconds (updateHabitName)
+        
+     console.log("Fetching the habit list from the server.");
+     $.getJSON(habits, updateHabitName());
+        
+}, 2000);
+
+setInterval(function() {                                        // retrieves a list of habits from the server every 2 seconds (updateHabitDays)
+    
+ console.log("Fetching the habit list from the server.");
+ $.getJSON(habits, updateHabitDays());
+    
+}, 2000);
+
+

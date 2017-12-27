34249e3eee611b1dd84d4dfb832dead8807b1b7c
BobDorland <33722105+BobDorland@users.noreply.github.com>
2017-12-27 20:07:04 +0100

    Back-upje

--- script.js
+++ script.js
@@ -21,17 +21,70 @@ function loginPrompt() {
     
 }
 
+function commentPrompt() {
+
+    window.open("addComment.html", "Window", "height=500, width=600");
+
+}
+
+function completedPrompt() {
+
+    window.open("habitCompleted.html", "Window", "height=500, width=600");
+
+}
+
 function addForm() {
+   
+    // TODO: add habit to the database (SQL)
 
     window.close();
+    
 }
 
 function deleteForm() {
-    //TODO delete selected Habits from database
+
+    // TODO: delete habit from database (SQL)
+
     window.close();
+
 }
 
-function editForm() {
-    //TODO edit a specific Habit
+function bestFirst() {
+
+// TODO: best above (SQL) 
+
+}
+
+function worstFirst() {
+
+// TODO: worst above (SQL)
+
+}
+
+function recentFirst() {
+
+// TODO: most recent above (SQL) 
+
+}
+
+function markCompletedForm() {
+
+    // TODO: mark a habit as completed
+
     window.close();
+
+}
+
+function editHabitForm() {
+
+    // TODO: update selected habit
+    window.close();
+
+}
+
+function addComment() {
+
+    // TODO: add a comment
+    window.close();
+
 }
